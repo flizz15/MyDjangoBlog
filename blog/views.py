128cea1cbc00f10337557b0d85b4e16d7b568cf3
Jan Sadownik <30599336+flizz15@users.noreply.github.com>
2020-02-03 21:48:18 +0100

    added comment system

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
 
 def post_list(request):
@@ -19,6 +19,20 @@ def post_detail(request, pk):
         raise Http404
     else:
         return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
 
 @login_required
@@ -78,3 +92,17 @@ def post_unpublish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.unpublish()
     return redirect('post_list')
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
